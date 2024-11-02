11fe3ae566174fbd0d96c0ea9a7ceb5baf5feedb
MoriumAkter123 <morium2312091068@diu.edu.bd>
2024-11-02 06:53:08 +0600

    mycode

---
+++ script.js
@@ -0,0 +1,51 @@
+
+// function sum (){
+    
+//     let x = 10;
+//     let y = 20;
+
+//     var result  = x+y;
+//     console.log("SUM", result);
+
+// }
+
+// sum ();
+
+
+function sub(number1,number2){
+
+    var x=number1;
+    var y=number2;
+    var result= x-y;
+    //console.log("SUB",result);//-36
+    return result;
+
+}
+function sum(number1,number2){
+    let x = number1;
+    let y = number2;
+    var result= x+y;
+    return result;
+}
+
+// function mult(number1,number2){
+//     let x = number1;
+//     let y = number2;
+//     var result= x*y;
+//     return result;
+// }
+
+//console.log(sub (54,90));
+let substraction = sub(300 , 200);
+console.log("substraction :",substraction);
+let secondSub = substraction + 120;
+console.log("secondSub :",secondSub);
+
+let thirdSub =(20,56);
+console.log("thirdSub :",thirdSub);
+
+let summation = sum(100,300);
+console.log("summation",summation);
+
+//  let product = mult(10,30);
+// console.log("Product",mult);
